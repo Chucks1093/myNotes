c3f463d592e668ac4f5e09a20d2baea99d4c6cb0
Sebastian Anioke <aniokechukwudi7@gmail.com>
2022-10-24 00:29:03 +0100

    finished adding 6 courses

--- data/courses.js
+++ data/courses.js
@@ -1,11 +1,15 @@
 import cedr314 from "./cedr 314.json";
-import chem321 from "./chem 321.json";
+import chem312 from "./chem 312.json";
 import mcb304 from "./mcb 304.json";
 import bch342 from "./bch 342.json";
+import bch352 from "./bch 352.json";
+import chem374 from "./chem 374.json";
 
 export default {
      "CEDR 314" : cedr314,
-     "CHEM 321" : chem321,
+     "CHEM 312" : chem312,
      "MCB 304" : mcb304,
-     "BCH 342" : bch342
+     "BCH 342" : bch342,
+     "BCH 352" : bch352,
+     "CHEM 374" : chem374
 };
