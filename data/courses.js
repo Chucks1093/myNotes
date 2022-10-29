b3a1f59ba1e61dc8f04d441abd53901da5f945d2
Sebastian Anioke <aniokechukwudi7@gmail.com>
2022-10-30 00:02:27 +0100

    changed location of file medias to public

--- data/courses.js
+++ data/courses.js
@@ -2,7 +2,7 @@ import cedr314 from "./cedr 314.json";
 import chem312 from "./chem 312.json";
 import mcb304 from "./mcb 304.json";
 import bch342 from "./bch 342.json";
-import bch352 from "./bch 352.json";
+// import bch352 from "./bch 352.json";
 import chem374 from "./chem 374.json";
 
 export default {
@@ -10,6 +10,5 @@ export default {
      "CHEM 312" : chem312,
      "MCB 304" : mcb304,
      "BCH 342" : bch342,
-     "BCH 352" : bch352,
      "CHEM 374" : chem374
 };
