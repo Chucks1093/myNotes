8ae42625056ea57cd8a5c2527e6c27f29455f88c
Sebastian Anioke <aniokechukwudi7@gmail.com>
2023-04-27 15:11:06 +0100

    add bch 401

--- src/courses/courses.js
+++ src/courses/courses.js
@@ -2,6 +2,8 @@ import BCH431 from "./BCH 431/deck.json";
 import BCH411 from "./BCH 411/deck.json";
 import BCH341 from "./BCH 341/deck.json";
 import BCH421 from "./BCH 421/deck.json";
+import BCH401 from "./BCH 401/deck.json";
+
 
 
 export default {
@@ -9,4 +11,6 @@ export default {
     "BCH 411" : BCH411,
     "BCH 341" : BCH341,
     "BCH 421" : BCH421,
+    "BCH 401" : BCH401
 };
+
