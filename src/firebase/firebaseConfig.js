fdfe9c4125c609b68ce6072942a473e7669579d8
Sebastian Anioke <aniokechukwudi7@gmail.com>
2023-07-02 19:08:24 +0100

    added firebase configuration

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+  
+initializeApp(firebaseConfig);
+
+const myNotes = getDatabase();
+
+export default myNotes;
---
+++ src/firebase/getNotes.js
@@ -0,0 +1,8 @@
+import myNotes from "./firebaseConfig";
+
+async function getNotes() {
+    // const resp = await myNotes.ref.;
+    console.log(myNotes)
+}
+
+export default getNotes;
